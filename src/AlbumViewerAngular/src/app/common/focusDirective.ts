d2220a4653a96c6b0d09e8af7d96dde0d1a003dc
sajid <sajid@snp.com>
2019-09-26 19:29:33 +0530

    Revert "Update client application"
    This reverts commit 7e8fe538e628e33b906bee10ca2d83b930184a7c.

--- src/AlbumViewerAngular/src/app/common/focusDirective.ts
+++ src/AlbumViewerAngular/src/app/common/focusDirective.ts
@@ -1,17 +1,16 @@
-import { Directive, Input, ElementRef, OnChanges } from '@angular/core';
+import { Directive, Input, ElementRef } from '@angular/core';
 
 @Directive({
-	selector : '[focus]'  // [focus]="true|false"
+	selector : '[focus]'
 })
-export class FocusDirective implements OnChanges {
+export class FocusDirective {
 	@Input() focus : boolean;
 
 	constructor(private element : ElementRef) {
-		
 	}
 	
-	ngOnChanges() {
+	protected ngOnChanges() {
 		if (this.focus)
-			setTimeout( ()=> this.element.nativeElement.focus() ,200);
+			this.element.nativeElement.focus();
 	}
 }
